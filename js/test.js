3e8ed381fc7627625a3369a3dcc694ee7311b405
BetterSoftBuilder <password9000>
2014-11-28 16:17:13 +0200

    model, controler

--- js/test.js
+++ js/test.js
@@ -1,24 +1,38 @@
-var Tasty = (function() {
-	var i=0;
+var TastyModel = (function() {
 	var tastyBase = [];
 	return {
-		init:   function(cover) {
-			    	var _this = this;
-			    	for (var backUrl in cover) {
-			    		cover[backUrl] && $('#' + backUrl.toString().substr(backUrl.toString().length - 3, 3)).css({'background' : "url(" + cover[backUrl] + ") no-repeat"});
-			    	}
-			    	$.getJSON('../json/info_box.json', function(json){
+		getData:   function(cover) {
+                                if (!tastyBase) {
+                                    when($.getJSON('../json/info_box.json')).then(
+                                    function(json){
 			    		tastyBase = json;
-			    		_this.load();
-			    		var h = parseInt($("#main_text").css("height"));
-			    		var tasty_box = $("#tasty_box");
-			    		tasty_box.on("click", "#prev", function(e) {e.preventDefault(); _this.navigation(0);});
-			    		tasty_box.on("click", "#next", function(e) {e.preventDefault(); _this.navigation(1);});
-			    		tasty_box.on("click", "#show", function(e) {e.preventDefault(); _this.showDetails(h);});
-			    	});
-			    },
+                                        return tastyBase;
+			    	    },function() {
+                                        //TODO
+                                    });
+                                } else {
+                                    return tastyBase;
+                                }
+			    }
+	};
+}());
 
-		load:   function(first) {
+var TastyController = (function(){
+    var model = pModel || TastyModel;
+    var i=0;
+    var h = parseInt($("#main_text").css("height"));
+    var tasty_box = $("#tasty_box");
+    return {
+           init: function(cover) {
+                for (var backUrl in cover) {
+		    cover[backUrl] && $('#' + backUrl.toString().substr(backUrl.toString().length - 3, 3)).css({'background' : "url(" + cover[backUrl] + ") no-repeat"});
+                }
+                tasty_box.on("click", "#prev", function(e) {e.preventDefault(); _this.navigation(0);});
+	        tasty_box.on("click", "#next", function(e) {e.preventDefault(); _this.navigation(1);});
+	        tasty_box.on("click", "#show", function(e) {e.preventDefault(); _this.showDetails(h);});
+           },
+
+           load:   function(first) {
 			    	$('#main_img').fadeOut(250, function() {
 			    		$(this).attr('src', 'img/' + tastyBase[i].img).fadeIn(500);
 			    	});
@@ -28,27 +42,29 @@ var Tasty = (function() {
 			    	$('#find').attr('href', tastyBase[i].productUrl);
 			    },
 
-		navigation: function(nav) {
-						if (nav) {
-							i++;
-							if (i>=tastyBase.length) i=0;
-						} else {
-							i--;
-							if (i<0) i=tastyBase.length-1;
-						};
-						this.load();
-					},
+           navigation: function(nav) {
+	    	if (nav) {
+	    		i++;
+	    		if (i>=tastyBase.length) i=0;
+	    	} else {
+	    		i--;
+	    		if (i<0) i=tastyBase.length-1;
+	    	};
+	    	this.load();
+	    },
 
-		showDetails: function(h) {
-						if ($("#main_text").css("height")==h+'px' || $("#main_text").css("height")==h+200+'px') {
-							$("#img_cont").stop().animate({ opacity: "toggle", height: 'toggle'}, 500);
-						}
-						if ($("#main_text").css("height")==h+'px') {
-							$("#main_text").stop().animate({ height: '+=200px'}, 500);
-						}
-						if ($("#main_text").css("height")==h+200+'px') {
-							$("#main_text").stop().animate({ height: h}, 500);
-						}
-					 }
-	}
+           showDetails: function(h) {
+		var text = $("#main_text");
+                var text_height = $("#main_text").css("height");
+                if (text_height==h+'px' || text_height==h+200+'px') {
+			$("#img_cont").stop().animate({ opacity: "toggle", height: 'toggle'}, 500);
+		}
+		if (text_height==h+'px') {
+			text.stop().animate({ height: '+=200px'}, 500);
+		}
+		if (text_height==h+200+'px') {
+			text.stop().animate({ height: h}, 500);
+		}
+	   }
+    };
 }());
