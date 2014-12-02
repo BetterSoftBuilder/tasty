583a77c0b373b5b43e559a4cbb17b241d973309a
BetterSoftBuilder <password9000>
2014-12-02 17:07:52 +0200

    fix

--- js/test.js
+++ js/test.js
@@ -1,59 +1,50 @@
 var TastyModel = (function() {
-	var tastyBase = [];
 	return {
-		getData:   function(cover) {
-                                if (!tastyBase) {
-                                    when($.getJSON('../json/info_box.json')).then(
+		getData:   function() {
+                                var _this = this;
+                                if (!this.tastyBase) {
+                                    $.when($.getJSON('../json/info_box.json')).then(
                                     function(json){
-			    		tastyBase = json;
-                                        return tastyBase;
+			    		_this.tastyBase = json;
 			    	    },function() {
                                         //TODO
                                     });
                                 } else {
-                                    return tastyBase;
+                                    return _this.tastyBase;
                                 }
-			    }
+			    },
+                getHeight: function() {
+                        this.h = parseInt($("#main_text").css("height"));
+                        this.tasty_box = $("#tasty_box");
+                }
 	};
 }());
 
 var TastyController = (function(){
-    var model = pModel || TastyModel;
+    var model = TastyModel;
     var i=0;
-    var h = parseInt($("#main_text").css("height"));
-    var tasty_box = $("#tasty_box");
-    return {
-           init: function(cover) {
-                for (var backUrl in cover) {
-		    cover[backUrl] && $('#' + backUrl.toString().substr(backUrl.toString().length - 3, 3)).css({'background' : "url(" + cover[backUrl] + ") no-repeat"});
-                }
-                tasty_box.on("click", "#prev", function(e) {e.preventDefault(); _this.navigation(0);});
-	        tasty_box.on("click", "#next", function(e) {e.preventDefault(); _this.navigation(1);});
-	        tasty_box.on("click", "#show", function(e) {e.preventDefault(); _this.showDetails(h);});
-           },
-
-           load:   function(first) {
-			    	$('#main_img').fadeOut(250, function() {
-			    		$(this).attr('src', 'img/' + tastyBase[i].img).fadeIn(500);
-			    	});
-			    	$('#title').html(tastyBase[i].title);
-			    	$('#text').html(tastyBase[i].description);
-			    	$('#details').html(tastyBase[i].note);
-			    	$('#find').attr('href', tastyBase[i].productUrl);
-			    },
-
-           navigation: function(nav) {
+    function _navigation(nav) {
 	    	if (nav) {
 	    		i++;
-	    		if (i>=tastyBase.length) i=0;
+	    		if (i>=model.tastyBase.length) i=0;
 	    	} else {
 	    		i--;
-	    		if (i<0) i=tastyBase.length-1;
+	    		if (i<0) i=model.tastyBase.length-1;
 	    	};
-	    	this.load();
-	    },
+	    	this._load();
+	    }
 
-           showDetails: function(h) {
+    function _load() {
+		$('#main_img').fadeOut(250, function() {
+			$(this).attr('src', 'img/' + model.tastyBase[i].img).fadeIn(500);
+		});
+		$('#title').html(model.tastyBase[i].title);
+		$('#text').html(model.tastyBase[i].description);
+		$('#details').html(model.tastyBase[i].note);
+		$('#find').attr('href', model.tastyBase[i].productUrl);
+            }
+
+    function _showDetails(h) {
 		var text = $("#main_text");
                 var text_height = $("#main_text").css("height");
                 if (text_height==h+'px' || text_height==h+200+'px') {
@@ -66,5 +57,18 @@ var TastyController = (function(){
 			text.stop().animate({ height: h}, 500);
 		}
 	   }
+
+    return {
+           init: function(cover) {
+                    for (var backUrl in cover) {
+                        cover[backUrl] && $('#' + backUrl.toString().substr(backUrl.toString().length - 3, 3)).css({'background' : "url(" + cover[backUrl] + ") no-repeat"});
+                    }
+                    model.getData();
+                    //_load();
+                    model.getHeight();
+                    tasty_box.on("click", "#prev", function(e) {e.preventDefault(); _navigation(0);});
+                    tasty_box.on("click", "#next", function(e) {e.preventDefault(); _navigation(1);});
+                    tasty_box.on("click", "#show", function(e) {e.preventDefault(); _showDetails(h);});
+                }
     };
 }());
