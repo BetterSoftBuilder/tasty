a8c232bc4601606948c487aa27754f9e1791137e
BetterSoftBuilder <dmitrij.stupak@privatbank.ua>
2015-02-09 15:24:30 +0200

    update

--- js/test.js
+++ js/test.js
@@ -1,19 +1,19 @@
-angular.module('tasty', [])
-    .controller('TastyBox', function ($scope) {
+var tastyApp = angular.module('tasty', ['ngMaterial','ngAnimate']);
 
-        $.fn.image = function(src, f) {
-            return this.each(function() {
-              var i = new Image();
-              i.src = src;
-              i.onload = f;
-              i.id = 'imgLoad';
-              this.appendChild(i);
-            });
-        };
+tastyApp.controller('TastyBox', ['$scope', '$interval', '$http', function($scope, $interval, $http) {
+
+        $scope.mode = 'query';
+        $scope.determinateValue = 30;
+        $interval(function() {
+          $scope.determinateValue += 1;
+          if ($scope.determinateValue > 100) {
+            $scope.determinateValue = 30;
+          }
+        }, 100, 0, true);
 
         $scope.tastyBase = false;
 
-        $scope.i = 0;
+        $scope.index = 0;
 
         $scope.cover = {
                             tasty_boxUrl: '../img/comp_plate_graybasic.png',
@@ -25,51 +25,33 @@ angular.module('tasty', [])
                             findHovUrl  : '../img/button_bg_orange_right3.png'
                         };
 
-        $scope._getData = function() {
-            $scope._loadAnimation($("#img_cont"));
+        $scope.getData = function() {
+            //$scope._loadAnimation($("#img_cont"));
             if (!$scope.tastyBase) {
-                $.when($.getJSON('../json/info_box.json')).then(
-                        function (json) {
-                            $scope.tastyBase = json;
-                            $("#imgLoad").hide();
-                            $scope._loadContent();
-                        }, function () {
-                    //TODO
-                });
+                $http.post('../json/info_box.json').success(
+                    function (json) {
+                        $scope.tastyBase = json;
+                    }).error(function () {
+                        //TODO
+                    });
             } else {
                 return $scope.tastyBase;
             }
         };
 
-        $scope._coverInit = function() {
+        $scope.coverInit = function() {
             for (var backUrl in $scope.cover) {
                 $scope.cover[backUrl] && $('#' + backUrl.toString().substr(backUrl.toString().length - 3, 3)).css({'background': "url(" + $scope.cover[backUrl] + ") no-repeat"});
             }
         };
 
         $scope._loadAnimation = function(container) {
-            container.image("img/loader.gif",function(){
-                var imgLoad = $("#imgLoad");
-                imgLoad.show();
-                var centerY = container.scrollTop() + (container.height() - imgLoad.height()) / 2;
-                var centerX = container.scrollLeft() + (container.width() - imgLoad.width()) / 2;
-                imgLoad.offset({top: centerY, left: centerX});
-            });
+            var centerY = container.scrollTop() + (container.height() - imgLoad.height()) / 2;
+            var centerX = container.scrollLeft() + (container.width() - imgLoad.width()) / 2;
+            imgLoad.offset({top: centerY, left: centerX});
         };
 
-        $scope._loadContent = function() {
-            $scope._loadAnimation($("#img_cont"));
-            $('#main_img').fadeOut(250, function () {
-                $(this).attr('src', 'img/' + $scope.tastyBase[$scope.i].img).fadeIn(500);
-                $("#imgLoad").hide();
-            });
-            $('#title').html($scope.tastyBase[$scope.i].title);
-            $('#text').html($scope.tastyBase[$scope.i].description);
-            $('#details').html($scope.tastyBase[$scope.i].note);
-            $('#find').attr('href', $scope.tastyBase[$scope.i].productUrl);
-        };
-
-        $scope._events = function() {
+        $scope.events = function() {
             var h = parseInt($("#main_text").css("height"));
             var tasty_box = $("#tasty_box");
             tasty_box.on("click", "#prev", function (e) {
@@ -88,19 +70,18 @@ angular.module('tasty', [])
 
         $scope._navigation = function(nav) {
             if (nav) {
-                $scope.i++;
-                if ($scope.i >= $scope.tastyBase.length) $scope.i = 0;
+                $scope.index++;
+                if ($scope.index >= $scope.tastyBase.length) $scope.index = 0;
             } else {
-                $scope.i--;
-                if ($scope.i < 0) $scope.i = $scope.tastyBase.length - 1;
+                $scope.index--;
+                if ($scope.index < 0) $scope.index = $scope.tastyBase.length - 1;
             }
-            $scope._loadContent();
-            //_loadAnimation($("#img_cont"));
+            //$scope._loadContent();
         };
 
         $scope._showDetails = function(h) {
-            var text = $("#main_text");
-            var text_height = $("#main_text").css("height");
+            var text = $(".main_text");
+            var text_height = $(".main_text").css("height");
             if (text_height == h + 'px' || text_height == h + 200 + 'px') {
                 $("#img_cont").stop().animate({opacity: "toggle", height: 'toggle'}, 500);
             }
@@ -112,10 +93,14 @@ angular.module('tasty', [])
             }
         };
 
-        $scope.init = function() {
-            $scope._coverInit();
-            $scope._getData();
-            $scope._events();
+        $scope.currentIndex = function(i) {
+            return $scope.index == i;
         };
 
-    });
+        $scope.init = function() {
+            $scope.coverInit();
+            $scope.getData();
+            $scope.events();
+        };
+
+    }]);
