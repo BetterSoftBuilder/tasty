9e3902189978de07867e9f54702d108cbe8d7280
BetterSoftBuilder <dmitrij.stupak@privatbank.ua>
2015-08-21 17:43:11 +0300

    update 1

--- js/test.js
+++ js/test.js
@@ -1,4 +1,4 @@
-var Tasty = (function () {
+function Tasty(options) {
 
     var i = 0;
     var tastyBase = false;
@@ -13,14 +13,15 @@ var Tasty = (function () {
         });
     };
 
-    function _getData() {
-        _loadAnimation($("#img_cont"));
+    function getData() {
+        loadAnimation($("#img_cont"));
+        coverInit();
         if (!tastyBase) {
             $.when($.getJSON('../json/info_box.json')).then(
                     function (json) {
                         tastyBase = json;
                         $("#imgLoad").hide();
-                        _loadContent();
+                        loadContent();
                     }, function () {
                 //TODO
             });
@@ -29,13 +30,14 @@ var Tasty = (function () {
         }
     }
 
-    function _coverInit(cover) {
+    function coverInit(cover) {
+        cover = cover || options.cover;
         for (var backUrl in cover) {
-            cover[backUrl] && $('#' + backUrl.toString().substr(backUrl.toString().length - 3, 3)).css({'background': "url(" + cover[backUrl] + ") no-repeat"});
+            cover[backUrl] && $('#' + backUrl.toString().substr(0, backUrl.toString().length - 3)).css({'background': "url(" + cover[backUrl] + ") no-repeat"});
         }
     }
 
-    function _loadAnimation(container) {
+    function loadAnimation(container) {
         container.image("img/loader.gif",function(){
             var imgLoad = $("#imgLoad");
             imgLoad.show();
@@ -45,8 +47,8 @@ var Tasty = (function () {
         });
     }
 
-    function _loadContent() {
-        _loadAnimation($("#img_cont"));
+    function loadContent() {
+        loadAnimation($("#img_cont"));
         $('#main_img').fadeOut(250, function () {
             $(this).attr('src', 'img/' + tastyBase[i].img).fadeIn(500);
             $("#imgLoad").hide();
@@ -57,24 +59,24 @@ var Tasty = (function () {
         $('#find').attr('href', tastyBase[i].productUrl);
     }
 
-    function _events() {
+    function events() {
         var h = parseInt($("#main_text").css("height"));
         var tasty_box = $("#tasty_box");
         tasty_box.on("click", "#prev", function (e) {
             e.preventDefault();
-            _navigation(0);
+            navigation(0);
         });
         tasty_box.on("click", "#next", function (e) {
             e.preventDefault();
-            _navigation(1);
+            navigation(1);
         });
         tasty_box.on("click", "#show", function (e) {
             e.preventDefault();
-            _showDetails(h);
+            showDetails(h);
         });
     }
 
-    function _navigation(nav) {
+    function navigation(nav) {
         if (nav) {
             i++;
             if (i >= tastyBase.length) i = 0;
@@ -82,11 +84,11 @@ var Tasty = (function () {
             i--;
             if (i < 0) i = tastyBase.length - 1;
         }
-        _loadContent();
+        loadContent();
         //_loadAnimation($("#img_cont"));
     }
 
-    function _showDetails(h) {
+    function showDetails(h) {
         var text = $("#main_text");
         var text_height = $("#main_text").css("height");
         if (text_height == h + 'px' || text_height == h + 200 + 'px') {
@@ -100,11 +102,31 @@ var Tasty = (function () {
         }
     }
 
-    return {
-        init: function (cover) {
-            _coverInit(cover);
-            _getData();
-            _events();
-        }
-    };
-}());
+    this.coverInit = coverInit;
+    this.getData = getData;
+    this.events = events;
+};
+
+var tasty = new Tasty({
+   cover:  {
+                tasty_boxUrl: '../img/comp_plate_graybasic.png',
+                prevUrl     : '../img/button_bg_white_left2.png',
+                nextUrl     : '../img/button_bg_white_right2.png',
+                findUrl     : '../img/button_bg_white_right3.png',
+                prevHovUrl  : '../img/button_bg_orange_left2.png',
+                nextHovUrl  : '../img/button_bg_orange_right2.png',
+                findHovUrl  : '../img/button_bg_orange_right3.png'
+            }
+});
+
+tasty.coverInit({
+                tasty_boxUrl: '../img/comp_plate_graybasic.png',
+                prevUrl     : '../img/button_bg_white_right2.png',
+                nextUrl     : '../img/button_bg_white_left2.png',
+                findUrl     : '../img/button_bg_white_right3.png',
+                prevHovUrl  : '../img/button_bg_orange_left2.png',
+                nextHovUrl  : '../img/button_bg_orange_right2.png',
+                findHovUrl  : '../img/button_bg_orange_right3.png'
+            });
+tasty.getData();
+tasty.events();
